be13672afd12b7cbc14c33fa4b13c37e4a3d0d8e
pvrodrigues10 <paulovitor-cr@hotmail.com>
2025-04-13 12:55:53 -0300

    feat: cria order models

---
+++ model/models.py
@@ -0,0 +1,5 @@
+class Order:
+    def __init__(self, orderid=None, customerid=None, employeeid=None):
+        self.orderid = orderid
+        self.customerid = customerid
+        self.employeeid = employeeid
