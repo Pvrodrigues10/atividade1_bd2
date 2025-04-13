2a9f2d4f75d80b4b0b59094dba4a588dc3b300c2
pvrodrigues10 <paulovitor-cr@hotmail.com>
2025-04-13 16:24:17 -0300

    feat: cria funcao segura de insert

--- dao/pedido_dao.py
+++ dao/pedido_dao.py
@@ -8,19 +8,44 @@ class InserePedido:
             
             with northwind.cursor() as sessao:              
                 sessao.execute(f"INSERT INTO northwind.orders (orderid, customerid, employeeid) VALUES ({pedido.orderid}, '{pedido.customerid}', {pedido.employeeid})")
-                
-                # Recupera o ID do pedido inserido
-                resultado = sessao.fetchone()
-                print(f"Pedido inserido com sucesso! ID do pedido: {resultado[0]}")
-                
+                             
                 # Commit da transação
-                northwind.commit()    
+                northwind.commit()
+                print("✅ Pedido inserido com sucesso!")    
                             
         except Exception as e:
             print(f"❌ Erro durante a inserção: {e}")
             if northwind:
-                sessao.close()
                 northwind.rollback()
+                sessao.close()
+                northwind.close()
+            return False
+        
+        finally:
+            if northwind:
+                sessao.close()
+                northwind.close()
+                return True
+            
+    
+    def inserePedidoSeguro(pedido):
+        northwind = None
+        try:
+            northwind = connect()
+            
+            with northwind.cursor() as sessao:              
+                sessao.execute(f"INSERT INTO northwind.orders (orderid, customerid, employeeid) VALUES (%s, %s, %s)", (pedido.orderid, pedido.customerid, pedido.employeeid))
+                
+                # Commit da transação
+                northwind.commit()
+                print("✅ Pedido inserido com sucesso!")
+                            
+        except Exception as e:
+            print(f"❌ Erro durante a inserção: {e}")
+            if northwind:
+                northwind.rollback()
+                sessao.close()
+                northwind.close()
             return False
         
         finally:
